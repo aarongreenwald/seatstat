ce5d769128139218ab4e4fd9519b3c9dfa5a09a9
Aaron Greenwald <aaron@aarongreenwald.com>
2014-09-07 18:54:40 -0400

    updated readme, a little progress getting this setting a simple demo set up

--- grouper/README.txt
+++ grouper/README.txt
@@ -4,11 +4,14 @@ grouper README
 Getting Started
 ---------------
 
-- cd <directory containing this file>
+Clone this repository and cd to the directory containing this file. Then:
 
-- $VENV/bin/python setup.py develop
+- mkdir ../env
+- virtualenv ../env
+- ../env/bin/python setup.py develop
+- ../env/bin/initialize_grouper_db development.ini
+- ../env/bin/pserve development.ini
 
-- $VENV/bin/initialize_grouper_db development.ini
+Have fun!
 
-- $VENV/bin/pserve development.ini
-
+(You may need to install some things along the way, like virtualenv. It should be self-explanatory, unless you're on a Windows system. Then I recommend switching operating systems or finding someone with more patience than I have.)
--- grouper/grouper/ui/home/home-controller.js
+++ grouper/grouper/ui/home/home-controller.js
@@ -8,20 +8,40 @@ bling.home.controller('HomeCtrl', ['$scope', '$http', function($scope, $http){
                         groupSize: public.groupSize             
                     }
                 })
+                .success(function(data){
+                    public.groups = data
+                })
             }
         }
         
         var public = {          
             generateGroups: private.generateGroups,            
             
-            members: [{name: 'Tom'}, {name: 'Dick'}],
+            members: [],
             illegalPairs: [],
-            groupSize: 4,
+            groupSize: 5,
             
             addMember: function(){
                 this.members.push({name: ''} )
             }
         }
+        
+        names = ['Liam', 'Olivia', 
+                'Noah', 'Emma', 
+                'Ethan', 'Sophia' ,
+                'Mason' ,'Ava',               
+                'Logan',    'Isabella',             
+                'Lucas',    'Mia',              
+                'Jacob',    'Charlotte',                
+                'Jackson',  'Emily',                
+                'Jack', 'Harper',               
+                'Aiden',    'Abigail',              
+                'Elijah',   'Avery']
+        
+        for (i in names){
+            public.members.push({name: names[i]})
+        }
+
         
         return public
     }
