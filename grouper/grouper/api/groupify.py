480817f3d95af729c2bbff9ad8be195ef23cd2a8
Aaron Greenwald <aaron@aarongreenwald.com>
2014-09-07 16:50:34 -0400

    added pyramid scaffold app and began working on itegrating grouping logic into an angular/pyramid app

--- grouper/grouper/api/groupify.py
+++ grouper/grouper/api/groupify.py
@@ -1,7 +1,10 @@
 from member_set import *
 from member import *
 
-names = ['Liam', 'Olivia', 'Noah', 'Emma', 'Ethan', 'Sophia' ,'Mason' ,'Ava',               
+names = ['Liam', 'Olivia', 
+            'Noah', 'Emma', 
+            'Ethan', 'Sophia' ,
+            'Mason' ,'Ava',               
             'Logan',    'Isabella',             
             'Lucas',    'Mia',              
             'Jacob',    'Charlotte',                
@@ -20,6 +23,11 @@ illegal_pairs = [
             [members[6], members[7]],
             [members[12], members[7]],           
             [members[8], members[9]],
+            [members[10], members[9]],
+            [members[11], members[21]],
+            [members[12], members[11]],
+            [members[14], members[13]],
+            [members[16], members[17]],
         ]
 
 member_set = MemberSet(members, 4, illegal_pairs)
---
+++ grouper/grouper/api/views.py
@@ -0,0 +1,67 @@
+from pyramid.response import Response
+from pyramid.view import view_config
+from pyramid.view import forbidden_view_config
+from pyramid.view import notfound_view_config
+from member_set import *
+from member import *
+
+import pyramid
+
+from .models import (
+    DBSession 
+    )
+
+
+def format(obj):    
+    if isinstance(obj, datetime.date):
+        return str(obj)
+    elif isinstance(obj, numbers.Number):
+        return str(obj) 
+    else:
+        return obj
+
+def serialize(result_set):          
+    if not isinstance(result_set, list):
+        result = {c.name: format(getattr(result_set, c.name)) for c in result_set.__table__.columns}
+    else:
+        result = []
+        for row in result_set:
+            serialized = {c.name: format(getattr(row, c.name)) for c in row.__table__.columns}
+            result.append(serialized)
+    return result
+
+@view_config(route_name='groups', renderer='json')
+def groups(request):    
+    
+    names = ['Liam', 'Olivia', 
+                'Noah', 'Emma', 
+                'Ethan', 'Sophia' ,
+                'Mason' ,'Ava',               
+                'Logan',    'Isabella',             
+                'Lucas',    'Mia',              
+                'Jacob',    'Charlotte',                
+                'Jackson',  'Emily',                
+                'Jack', 'Harper',               
+                'Aiden',    'Abigail',              
+                'Elijah',   'Avery']
+
+    members = []
+    for i in range(0, len(names)):
+        members += [{'name': Member(names[i])}]
+
+    illegal_pairs = [
+                [members[0], members[2]],
+                [members[3], members[4]],
+                [members[6], members[7]],
+                [members[12], members[7]],           
+                [members[8], members[9]],
+                [members[10], members[9]],
+                [members[11], members[21]],
+                [members[12], members[11]],
+                [members[14], members[13]],
+                [members[16], members[17]],
+            ]
+
+    member_set = MemberSet(members, 4, illegal_pairs)
+    return member_set.groupify()   
+    
