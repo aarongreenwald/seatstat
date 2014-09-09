d9c432f5ee08a0ab9d31cf8a387aa66fd7b8b377
Aaron Greenwald <aaron@aarongreenwald.com>
2014-09-08 21:20:43 -0400

    round-trip groupify works at a very basic level

--- grouper/grouper/api/views.py
+++ grouper/grouper/api/views.py
@@ -5,6 +5,8 @@ from pyramid.view import notfound_view_config
 from member_set import *
 from member import *
 
+import json
+
 import pyramid
 
 from .models import (
@@ -21,39 +23,32 @@ def format(obj):
         return obj
 
 def serialize(result_set):          
-    if not isinstance(result_set, list):
+    if not isinstance(result_set, list):        
         result = {c.name: format(getattr(result_set, c.name)) for c in result_set.__table__.columns}
     else:
         result = []
-        for row in result_set:
+        for row in result_set:          
             serialized = {c.name: format(getattr(row, c.name)) for c in row.__table__.columns}
             result.append(serialized)
     return result
 
-@view_config(route_name='groups', renderer='json')
+@view_config(route_name='groups', renderer='json') #using the json renderer double escapes the strings
 def groups(request):    
-    
-    names = request.params.getall('members')
+        
+    names = request.params.getall('members')    
     group_size = int(request.params['groupSize'])
 
     members = []    
     for i in range(0, len(names)):
         members += [Member(names[i])]
 
-    illegal_pairs = [
-                [members[0], members[2]],
-                [members[3], members[4]],
-                [members[6], members[7]],
-                [members[12], members[7]],           
-                [members[8], members[9]],
-                [members[10], members[9]],
-                [members[11], members[21]],
-                [members[12], members[11]],
-                [members[14], members[13]],
-                [members[16], members[17]],
-            ]
-
+    illegal_pairs_names = request.params.getall('illegalPairs')
+    
     illegal_pairs = []
-    member_set = MemberSet(members, group_size, illegal_pairs)
+    for pair_str in illegal_pairs_names:        
+        pair = json.loads(pair_str)               
+        illegal_pairs += [[Member(pair[0]), Member(pair[1])]]
+        
+    member_set = MemberSet(members, group_size, illegal_pairs)    
     return member_set.groupify()   
     
