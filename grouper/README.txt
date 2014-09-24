cf6bb34c2350a975e4b9746b784a02713e6994f2
Aaron Greenwald <aaron@aarongreenwald.com>
2014-09-24 08:35:16 -0400

    update setup.py

---
+++ grouper/README.txt
@@ -0,0 +1,2 @@
+TODO - this is the readme for the python project. I'd prefer to use the readme a directory up
+in the setup.py, though.
--- grouper/setup.py
+++ grouper/setup.py
@@ -5,8 +5,6 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.txt')) as f:
     README = f.read()
-with open(os.path.join(here, 'CHANGES.txt')) as f:
-    CHANGES = f.read()
 
 requires = [
     'pyramid',
@@ -21,19 +19,18 @@ requires = [
     ]
 
 setup(name='grouper',
-      version='0.0',
-      description='grouper',
-      long_description=README + '\n\n' + CHANGES,
+      version='0.1',
+      description='grouper is a web api for dividing groups of items into smaller groups while honoring constraints',
+      long_description=README,
       classifiers=[
         "Programming Language :: Python",
         "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "Topic :: Web Application :: Web API",
+        "Intended Audience :: School Teachers",
         ],
       author='Aaron Greenwald',
       author_email='aaron@aarongreenwald.com',
-      url='http://www.aarongreenwald.com',
-      keywords='web wsgi bfg pylons pyramid',
+      url='www.aarongreenwald.com',      
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
@@ -42,7 +39,5 @@ setup(name='grouper',
       entry_points="""\
       [paste.app_factory]
       main = grouper:main
-      [console_scripts]
-      initialize_grouper_db = grouper.scripts.initializedb:main
       """,
       )
