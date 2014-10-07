e512585386752cacbe1dbb557bfe407175ef8450
Aaron Greenwald <aaron@aarongreenwald.com>
2014-10-06 23:59:48 -0400

    add home page placeholder

--- grouper/grouper/views.py
+++ grouper/grouper/views.py
@@ -6,7 +6,6 @@ def layout():
     layout = renderer.implementation().macros['layout']
     return layout
 
-
 @view_config(renderer="ui/seatstat.pt", route_name="app")
 def index_view(request):
     return {"layout": layout(),
@@ -16,5 +15,9 @@ def index_view(request):
 @view_config(renderer="ui/about.pt", route_name="about")
 def about_view(request):
     return {"layout": layout(),
-            "page_title": "About Grouper"}
+            "page_title": "About SeatStat"}
 
+@view_config(renderer="ui/home.pt", route_name="home")
+def index_view(request):
+    return {"layout": layout(),
+            "page_title": "SeatStat"}
