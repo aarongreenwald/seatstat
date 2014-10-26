b1faa29f74bf652c70f43b9fb5e4542cc2f11f6f
Aaron Greenwald <aaron@aarongreenwald.com>
2014-10-26 13:28:30 -0400

    Add indication in UI when placements fail
    If SeatStat cannot find a good seating chart for the
    restrictions, the students for whom placements could
    not be found will be colored red. This is a temporary
    solution for the problem, and isn't very clear to users.
    But I doubt users will encounter this in normal usage.
    I need to do some more testing.

--- src/seatstat/api/member.py
+++ src/seatstat/api/member.py
@@ -1,17 +1,19 @@
+import json
 
 class Member:
     
     def __init__(self, name):
         self.name = name
+        self.placement_valid = True
         
     def __str__(self):
         return self.name
 
     def __repr__(self):
-        return self.name
+        return self.name 
 
     def __json__(self, request):
-        return self.name
+        return { 'name': self.name, 'placementValid': self.placement_valid }
         
     def __eq__(self, other):
         return (isinstance(other, self.__class__) 
