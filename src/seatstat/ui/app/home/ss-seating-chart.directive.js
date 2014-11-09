a6c94c75d7ad49c78569f71c509a4588c6900376
Aaron Greenwald <aaron@aarongreenwald.com>
2014-11-09 14:37:04 -0500

    Improvements to drag and drop
    Still not complete, but looks better than it
    did before.

--- src/seatstat/ui/app/home/ss-seating-chart.directive.js
+++ src/seatstat/ui/app/home/ss-seating-chart.directive.js
@@ -18,20 +18,44 @@ seatstat.home.directive('ssSeatingChart', ['ssSeatingChartDnD', 'class', functio
             ssSeatingChart: '='
         },
         link: function(scope, element, attributes){
-            var key = scope.ssSeatingChart.key
+            var dragDropKey = scope.ssSeatingChart.dragDropKey
+            
             var dragStart = function(event, b, c){                                
-                ssSeatingChartDnD.source = this                                
+                ssSeatingChartDnD.source = this
+                this.classList.add('ss-home-seating-chart-dragging')                                
+            }
+               
+            var dragOver = function(event){                    
+                this.classList.add('ss-home-seating-chart-dragging') 
+            }  
+            
+            var dragEnter = function(event){
+                ssSeatingChartDnD.destination = event.currentTarget                               
+                
+            }
+            
+            var dragLeave = function(event){
+                this.classList.remove('ss-home-seating-chart-dragging') 
+                //TODO - this is necessary but isn't working
+                //without it, dragging over an element and then moving
+                //to no element will still perform the swap
+                //ssSeatingChartDnD.destination = null                               
+                
             }
                                                    
             var dragEnd = function(event){                 
-                if (ssSeatingChartDnD.source !== ssSeatingChartDnD.destination){ 
-                    var sourceStudent = ssSeatingChartDnD.source.attributes['data-key'].value   
-                    var destinationStudent = ssSeatingChartDnD.destination.attributes['data-key'].value
+                if (ssSeatingChartDnD.destination && ssSeatingChartDnD.source !== ssSeatingChartDnD.destination){ 
+                    var sourceStudent = ssSeatingChartDnD.source.attributes['data-dragdrop-key'].value   
+                    var destinationStudent = ssSeatingChartDnD.destination.attributes['data-dragdrop-key'].value
                     scope.$apply(function(){
-                        var temp
+                        var temp //placeholder for the swap
                         for (var i in $class.seatingChart){
                             var table = $class.seatingChart[i]
                             for (var j in table){
+                                //for both the source and the destination
+                                //(because we don't know which we'll hit first)
+                                //we need to check either place the item in temp
+                                //and find the other item or just take the item out of temp
                                 if (table[j].name === sourceStudent){
                                     if (!temp){
                                         temp = table[j]
@@ -53,23 +77,21 @@ seatstat.home.directive('ssSeatingChart', ['ssSeatingChartDnD', 'class', functio
                                 }
                             }
                         }
+                        //reset the validation
                         $class.validation.validateSeatingChart($class.seatingChart)    
-                    })                
-                    
-                   // var temp = ssSeatingChartDnD.destination.innerHTML
-                   // ssSeatingChartDnD.destination.innerHTML = ssSeatingChartDnD.source.innerHTML
-                   // ssSeatingChartDnD.source.innerHTML = temp
+                    })                                    
                 }                                
+                ssSeatingChartDnD.source.classList.remove('ss-home-seating-chart-dragging')                
             }
                     
-            var dragOver = function(event){                    
-                ssSeatingChartDnD.destination = event.currentTarget                               
-            }          
-            element.attr('data-key', key)
+                  
+            element.attr('data-dragdrop-key', dragDropKey)
             element.attr('draggable', 'true')                 
             element.bind('dragstart', dragStart)              
             element.bind('dragover', dragOver)                            
             element.bind('dragend', dragEnd)
+            element.bind('dragenter', dragEnter)
+            element.bind('dragleave', dragLeave)
            
                     
         }
